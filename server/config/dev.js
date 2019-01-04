c31e4e923df9afde7f8420070d2f06c44d1f158b
Тулин Виктор Владимирович <VVTulin.OUT@sberbank.ru>
2019-01-04 11:38:18 +0300

    deleted server config file

--- server/config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    DB_URI: 'mongodb://Test:test12345@ds063779.mlab.com:63779/bwm-ng-dev',
-    SECRET: 'ada45da4a4',
-    AWS_ACCESS_KEY_ID: 'AKIAJAE7VU6HNCSJ4XGQ',
-    AWS_SECRET_ACCESS_KEY: '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'
-};
